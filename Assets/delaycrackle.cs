af60e5586d51a8f1972880520aa025908a281a26
otherchris <chriscaragianis@gmail.com>
2019-10-21 00:17:37 -0400

    wow

---
+++ Assets/delaycrackle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delaycrackle : MonoBehaviour
+{ 
+	void Start()
+	{
+		StartCoroutine(Delay());
+	}
+
+	IEnumerator Delay()
+	{
+		print(Time.time);
+		AudioSource auddio = GetComponent<AudioSource>();
+        yield return new WaitForSeconds(5);
+		auddio.Play();
+		print(Time.time);
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/start_anim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_anim : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        Animator animator = GetComponent<Animator>();
+        yield return new WaitForSeconds(5);
+        animator.Play("New Animation");
+        print(Time.time);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
